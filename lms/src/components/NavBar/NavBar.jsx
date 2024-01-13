d10f8462810f21e76414f80261a6ba4cd43e55f0
Ali Hamza <73699762+Ravian454@users.noreply.github.com>
2024-01-13 14:52:33 +0500

    added the menu

--- lms/src/components/NavBar/NavBar.jsx
+++ lms/src/components/NavBar/NavBar.jsx
@@ -3,6 +3,8 @@ import {
   faQuestionCircle,
   faBars,
   faGlobe,
+  faSignOutAlt,
+  faCog,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
@@ -12,65 +14,68 @@ function Navbar() {
   const [showMenu, setShowMenu] = useState(false);
 
   const toggleMenu = () => {
-    setShowMenu(!showMenu);
+    setShowMenu(prevState => !prevState);
   };
 
-  
 
   return (
-    <div>
-      <div className="flex justify-between items-center fixed top-0 w-full bg-white border-b py-3 px-6 z-20">
-        <div className="flex ">
-         <h2>DashBoard</h2>
-        </div>
-
-        <div className="relative">
-          <div className="lg:hidden cursor-pointer p-2 ml-[400px]" onClick={toggleMenu}>
-            <FontAwesomeIcon icon={faBars} className="text-gray-400" />
+      <div>
+        <div className="flex justify-between items-center fixed top-0 w-full bg-white border-b py-3 px-6 z-20">
+          <div className="flex">
+            <h2>DashBoard</h2>
           </div>
-          {/* Dropdown Content */}
-          {showMenu && (
-            <div className="lg:hidden absolute right-0 mt-2 bg-white rounded-md shadow-md text-left">
-              <Link to="/" className="block px-4 py-2 text-sm text-gray-700">
-                <FontAwesomeIcon icon={faGlobe} className="mr-2" /> Dashboard
-              </Link>
-              <Link
-                to="/profile"
-                className="block px-4 py-2 text-sm text-gray-700"
-              >
-                <FontAwesomeIcon icon={faGlobe} className="mr-2" /> profile
-              </Link>
-              <Link
-                to="/users"
-                className="block px-4 py-2 text-sm text-gray-700"
-              >
-                <FontAwesomeIcon icon={faGlobe} className="mr-2" /> Users
-              </Link>
-              <Link to="/" className="block px-4 py-2 text-sm text-gray-700">
-                <FontAwesomeIcon icon={faGlobe} className="mr-2" /> Files
-              </Link>
-              {/* Add other links here */}
+
+          <div className="relative">
+            <div
+                className="lg:hidden cursor-pointer p-2 ml-[400px]"
+                onClick={toggleMenu}
+            >
+              <FontAwesomeIcon icon={faBars} className="text-gray-400" />
             </div>
-          )}
-        </div>
-        <div className="flex items-center">
-          <FontAwesomeIcon
-            icon={faBell}
-            className="text-gray-400 w-[25px] h-[25px] p-2"
-          />
-          <FontAwesomeIcon
-            icon={faQuestionCircle}
-            className="text-gray-400 w-[25px] h-[25px] p-2"
-          />
-          <img
-            src="https://www.shareicon.net/data/512x512/2016/07/26/802043_man_512x512.png"
-            alt="avatar.png"
-            className="p-2  w-[44px] h-[44px]"
-          />
-          <h5 className="mt-2">Username</h5>
+
+            <div className="flex items-center relative">
+              <FontAwesomeIcon
+                  icon={faBell}
+                  className="text-gray-400 w-[25px] h-[25px] p-2"
+              />
+              <FontAwesomeIcon
+                  icon={faQuestionCircle}
+                  className="text-gray-400 w-[25px] h-[25px] p-2"
+              />
+              <img
+                  src="https://www.shareicon.net/data/512x512/2016/07/26/802043_man_512x512.png"
+                  alt="avatar.png"
+                  className="p-2 w-[44px] h-[44px]"
+              />
+              <div className="relative">
+                <h5 className="mt-2 cursor-pointer" onClick={toggleMenu}>
+                  Username
+                </h5>
+
+                {/* Dropdown Content */}
+                {showMenu && (
+                    <div className="absolute top-full right-0 mt-2 bg-white rounded-md shadow-md text-left w-[200px]">
+                      <Link
+                          to="/"
+                          className="block px-4 py-2 text-sm text-gray-700"
+                          onClick={toggleMenu}
+                      >
+                        <FontAwesomeIcon icon={faSignOutAlt} className="mr-2" /> Signout
+                      </Link>
+                      <Link
+                          to="/profile"
+                          className="block px-4 py-2 text-sm text-gray-700"
+                          onClick={toggleMenu}
+                      >
+                        <FontAwesomeIcon icon={faCog} className="mr-2" /> Settings
+                      </Link>
+                    </div>
+                )}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
   );
 }
 
